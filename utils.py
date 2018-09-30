136286bb095aa403159e6176ca7d5e12b792691c
Deeemi <davidedimeco@gmail.com>
2018-09-30 19:36:10 +0200

    Correction on input data + added discrimination rate

--- utils.py
+++ utils.py
@@ -32,9 +32,42 @@ def buildDict(keys, values):
 
 def build():
     keys = defineDict('./file/inputHeading.txt')
-    values = inputReader('./file/input.csv')
+    values = inputReader('./file/input_long.csv')
     return buildDict(keys, values)
 
 
 def field_filter(dictionary, field, attribute):
     return [i for i in dictionary if i[field] == attribute]
+
+
+def find_field_attribute(field, dictionary):
+    tmp = []
+    count = 0
+    for obj in dictionary:
+        tmp.append(obj[field])
+    obj = {field: set(tmp)}
+    return obj
+
+
+built = build()
+dimension = len(built)
+over50k = 0
+under50k = 0
+
+
+for att in list(find_field_attribute('race', built).values()):
+    for i in att:
+        print(i)
+        over50k = 0
+        under50k = 0
+        for j in built:
+            if j['race'] == i:
+                if j['result'] == '>50K':
+                    over50k = over50k + 1
+                else:
+                    under50k = under50k + 1
+        print("VALUE:" + str(i) + ", OVER 50K: " +
+              str(over50k) + ", UNDER 50K: " + str(under50k) + ", TOTAL: " + str(over50k + under50k) + " TASSO OVER 50K: " +
+              str(float(over50k)/(float(over50k) + float(under50k))) + " TASSO UNDER 50K: " +
+              str(float(under50k)/(float(over50k) + float(under50k))))
+        print("********")
