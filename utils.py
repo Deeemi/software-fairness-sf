6e5bf3d90ed963bc608f33344380a01a050486ba
Deeemi <davidedimeco@gmail.com>
2018-11-04 12:54:20 +0100

    added mapping for non-numeric values

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import csv
-
+import pandas as pd
 
 def inputReader(inputCsv):
     with open(inputCsv, 'rb') as csvfile:
@@ -8,7 +8,6 @@ def inputReader(inputCsv):
         for row in spamreader:
             inputRead.append(row)
     return inputRead
-
 
 def defineDict(inputData):
     file = open(inputData)
@@ -19,55 +18,64 @@ def defineDict(inputData):
         dictionary.append(i)
     return dictionary
 
-
 def buildDict(keys, values):
     dictionary = []
     for value in values:
         dictionary.append(dict(zip(keys, value)))
     return dictionary
 
-
-'''ENTRY-POINT'''
-
-
-def build():
-    keys = defineDict('./file/inputHeading.txt')
-    values = inputReader('./file/input_long.csv')
+def build(keys, values):
     return buildDict(keys, values)
-
 
 def field_filter(dictionary, field, attribute):
     return [i for i in dictionary if i[field] == attribute]
-
 
 def find_field_attribute(field, dictionary):
     tmp = []
     count = 0
     for obj in dictionary:
+        print(field)
         tmp.append(obj[field])
     obj = {field: set(tmp)}
     return obj
 
+def columnReader(fileName):
+    file = open(fileName)
+    lines = file.readlines()
+    dictionary = []
+    for i in lines:
+        i = i.replace('\n', '')
+        dictionary.append(i)
+    return dictionary
 
-built = build()
-dimension = len(built)
-over50k = 0
-under50k = 0
+'''input: csvFile = path al file csv che contiene i dati da mappare
+          headingFile = path al file txt che contiene il nome dei campi del file csv'''
+def field_mapping(csvFile, headingFile):
+    fields = columnReader(headingFile)
+    df = pd.read_csv(csvFile, skipinitialspace=True, usecols=fields)
+    mapped_fields = []
+    obj = {}
+    for column in df:
+        if(len(set(df.loc[: , column]))<=10):
+            obj = {column: {}}
+            for i in list(set(df.loc[: , column])):
+                obj[column].update({list(set(df.loc[: , column])).index(i): i})
+            mapped_fields.append(obj)
+    return mapped_fields
 
 
-for att in list(find_field_attribute('race', built).values()):
-    for i in att:
-        print(i)
-        over50k = 0
-        under50k = 0
-        for j in built:
-            if j['race'] == i:
-                if j['result'] == '>50K':
-                    over50k = over50k + 1
-                else:
-                    under50k = under50k + 1
-        print("VALUE:" + str(i) + ", OVER 50K: " +
-              str(over50k) + ", UNDER 50K: " + str(under50k) + ", TOTAL: " + str(over50k + under50k) + " TASSO OVER 50K: " +
-              str(float(over50k)/(float(over50k) + float(under50k))) + " TASSO UNDER 50K: " +
-              str(float(under50k)/(float(over50k) + float(under50k))))
-        print("********")
+def data_transofmation(inputHeading, inputCsv):
+    fields = columnReader(inputHeading)
+    df = pd.read_csv(inputCsv, skipinitialspace=True, usecols=fields)
+    mappedFields = field_mapping(inputCsv, inputHeading)
+    for column in df:
+        for current_field in mappedFields:
+            if(list(set(current_field))[0] == column):
+                for element in df.loc[: , column]:
+                    for i in list(current_field.values()):
+                        if(element in i.values()):
+                            df.loc[df[column] == element, column] = list(i.keys())[list(i.values()).index(element)]
+    return df
+
+
+                        
