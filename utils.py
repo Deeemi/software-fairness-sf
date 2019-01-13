1df26b5fad9df9e51f9c8ba4ddccc56a8d2ca961
Deeemi <davidedimeco@gmail.com>
2019-01-13 16:56:25 +0100

    Mapping correction and casual DAG
    The mapping feature is now corrected using pandas (trasformation of
    non-categorical variables to compute correlation) and was introduced
    the function to compute a casual DAG based on correlation between
    variables

--- utils.py
+++ utils.py
@@ -1,23 +1,7 @@
 import csv
 import pandas as pd
+import numpy as numpy
 
-'''def input_reader(inputCsv):
-    with open(inputCsv, 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=',')
-        inputRead = []
-        for row in spamreader:
-            inputRead.append(row)
-    return inputRead
-
-def define_dict(inputData):
-    file = open(inputData)
-    lines = file.readlines()
-    dictionary = []
-    for i in lines:
-        i = i.replace('\n', '')
-        dictionary.append(i)
-    return dictionary
-'''
 def build_dict(keys, values):
     dictionary = []
     for value in values:
@@ -34,7 +18,6 @@ def find_field_attribute(field, dictionary):
     tmp = []
     count = 0
     for obj in dictionary:
-        print(field)
         tmp.append(obj[field])
     obj = {field: set(tmp)}
     return obj
@@ -56,7 +39,7 @@ def field_mapping(csvFile, headingFile):
     mapped_fields = []
     obj = {}
     for column in df:
-        if(len(set(df.loc[: , column]))<=10):
+        if(len(set(df.loc[: , column]))<=50):
             obj = {column: {}}
             for i in list(set(df.loc[: , column])):
                 obj[column].update({list(set(df.loc[: , column])).index(i): i})
@@ -65,19 +48,103 @@ def field_mapping(csvFile, headingFile):
 
 
 def data_transofmation(inputHeading, inputCsv):
+    print("The software is mapping the datas")
     fields = column_reader(inputHeading)
     df = pd.read_csv(inputCsv, skipinitialspace=True, usecols=fields)
-    mappedFields = field_mapping(inputCsv, inputHeading)
-    for column in df:
-        for current_field in mappedFields:
-            if(list(set(current_field))[0] == column):
-                for element in df.loc[: , column]:
-                    for i in list(current_field.values()):
-                        if(element in i.values()):
-                            df.loc[df[column] == element, column] = list(i.keys())[list(i.values()).index(element)]
+    categories = list(df.columns.values)
+    for column in categories:
+        df[column] = df[column].astype('category')
+        cat_columns = df.select_dtypes(['category']).columns
+        df[cat_columns] = df[cat_columns].apply(lambda x: x.cat.codes)
+    print("Mapping complete!")
     return df
 
-df = data_transofmation('./file/inputHeading_loan.txt', './file/Loan_payments_data.csv')
-correlation = df.corr('pearson')
-print(correlation)
-                        
+def find_dag(datafame, field, fields):
+    print("The software is finding the maximum correlation for "+ field)
+    returnVal = ""
+    if(len(fields) == 0):
+        return returnVal
+    max = -1
+    for j in correlation:        
+        if(correlation[field][j]>max):
+            if(j != field):
+                max = correlation[field][j]
+                max_field = j
+                returnVal = dict({"field_1": field, "field_2": max_field, "correlation": max})
+            else:
+                continue
+    return returnVal
+
+
+datas = data_transofmation('./file/inputHeading.txt','./file/input_long.csv' )
+print("Start analizing the dataset...")
+
+print('-----PEARSON-----')
+correlation = datas.corr('pearson')
+fields = []
+for field in correlation:
+    fields.append(field)
+casual_dag = []
+
+first_node = find_dag(correlation, "result", fields)
+casual_dag.append(first_node["field_1"])
+field = first_node["field_2"]
+datas.drop("result", axis = 1, inplace=True)
+fields.remove("result")
+i = len(fields)
+
+while len(fields)>=2: 
+    correlation = datas.corr('kendall')
+    node = find_dag(correlation, field, fields)
+    casual_dag.append(node["field_1"])
+    field = node["field_2"]
+    datas.drop(node["field_1"], axis = 1, inplace=True)
+    fields.remove(node["field_1"])
+
+casual_dag.append(fields[0])
+result_string = ""
+
+for i in casual_dag:
+    result_string = result_string + "-->" + i
+
+result_string = result_string.replace("-->", "", 1)
+
+print("Finish!")
+print("The casual dag computet using correlation is: ")
+print(result_string)
+
+print('-----KENDALL-----')
+datas = data_transofmation('./file/inputHeading.txt','./file/input_long.csv' )
+print("Start analizing the dataset...")
+correlation = datas.corr('kendall')
+fields = []
+for field in correlation:
+    fields.append(field)
+casual_dag = []
+
+first_node = find_dag(correlation, "result", fields)
+casual_dag.append(first_node["field_1"])
+field = first_node["field_2"]
+datas.drop("result", axis = 1, inplace=True)
+fields.remove("result")
+i = len(fields)
+
+while len(fields)>=2: 
+    correlation = datas.corr('kendall')
+    node = find_dag(correlation, field, fields)
+    casual_dag.append(node["field_1"])
+    field = node["field_2"]
+    datas.drop(node["field_1"], axis = 1, inplace=True)
+    fields.remove(node["field_1"])
+
+casual_dag.append(fields[0])
+result_string = ""
+
+for i in casual_dag:
+    result_string = result_string + "-->" + i
+
+result_string = result_string.replace("-->", "", 1)
+
+print("Finish!")
+print("The casual dag computet using correlation is: ")
+print(result_string)
